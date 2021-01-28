d68895fcb33f235bbc364ddce8b8d9a771fd7a80
olga gomelskiy <olichka23.85@gmail.com>
2021-01-28 13:11:00 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -5,21 +5,10 @@
 # suite_d: "Charlie Chaplin",
 # suite_e: "Crumpet the Elf"
 # }
-def our_each(our_hash)
-  #[1,2,43,534,62,34,234,1]
-  for el in our_hash
-    #el - array
-    #el[0] - key
-    #el[1] - value
-    yield(el[0],el[1])
-  end
-end
+
 def select_winner(passengers)
-  winner = ""
-  #passengers.each() { |suite, name|
-  #  winner = name if suite == :suite_a && name.start_with?("A")
-  #}
-  our_each(passengers) |suite, name|
+  #winner = ""
+  passengers.each() { |suite, name|
     winner = name if suite == :suite_a && name.start_with?("A")
   }
 
