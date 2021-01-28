cf189df8878c556fff9ae470f23a99d62739ab00
olga gomelskiy <olichka23.85@gmail.com>
2021-01-28 12:40:04 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -8,10 +8,10 @@
 
 def select_winner(passengers)
   winner = ""
-  passengers.each do |suite, name|
+  passengers.each { |suite, name|
     if suite == :suite_a && name.start_with?("A")
       winner = name
     end
-  end# add the code snippet here!
+  }
   winner
 end
